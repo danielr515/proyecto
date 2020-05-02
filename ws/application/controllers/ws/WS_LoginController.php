dfc071769117ae35da537a76bb450cacf8adc034
Daniel Rodriguez <daniel.rodriguez.iglesias9@gmail.com>
2020-05-02 23:57:26 +0200

    .

--- ws/application/controllers/ws/WS_LoginController.php
+++ ws/application/controllers/ws/WS_LoginController.php
@@ -14,26 +14,37 @@ class WS_PruebaController extends RestController {
     }
 
     protected function loginAdmin_get() {
-        $uname =  $this->get( 'uname' );
-        $passwd = $this->get( 'passwd' );
-        $retmsg = '';
-        $code = '';
-        if ( !isset( $uname ) || !isset( $passwd ) ) {
-            $restmsg = 'Falta nombre de usuario o contraseña.';
-            $code = RestController::HTTP_BAD_REQUEST;
-        } else {
-            $user = $this->admin->getUserByLoginData( $uname, $passwd );
-            if ( $user->getUname() != '' ) {
-                $restmsg = 'Login correcto';
-                $code = RestController::HTTP_OK;
-            } else {
-                $restmsg = 'Datos incorectos';
-                $code = RestController::HTTP_UNAUTHORIZED;
-            }
-        }
+        // $uname =  $this->get( 'uname' );
+        // $passwd = $this->get( 'passwd' );
+        // $retmsg = '';
+        // $code = '';
+        // if ( !isset( $uname ) || !isset( $passwd ) ) {
+        //     $restmsg = 'Falta nombre de usuario o contraseña.';
+        //     $code = RestController::HTTP_BAD_REQUEST;
+        // } else {
+        //     $user = $this->admin->getUserByLoginData( $uname, $passwd );
+        //     if ( $user->getUname() != '' ) {
+        //         $restmsg = 'Login correcto';
+        //         $code = RestController::HTTP_OK;
+        //     } else {
+        //         $restmsg = 'Datos incorectos';
+        //         $code = RestController::HTTP_UNAUTHORIZED;
+        //     }
+        // }
+        // // $this->setHeaders();
+        // $this->response( $retmsg, $code );
+        $retmsg = [
+            [
+                'id' => 1,
+                'nombre' => 'prueba'
+            ],
+            [
+                'id' => 2,
+                'nombre' => 'otro'
+            ]
+        ];
         // $this->setHeaders();
-        $this->response( $retmsg, $code );
-        return 'a';
+        $this->response( $retmsg, RestController::HTTP_OK );
     }
 
     protected function setHeaders( $token = null ) {
