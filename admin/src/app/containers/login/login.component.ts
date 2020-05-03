a756c0de48599e1b9e17a40dd5f02b269f5e7b12
Daniel Rodriguez <daniel.rodriguez.iglesias9@gmail.com>
2020-05-04 00:16:45 +0200

    .

--- admin/src/app/containers/login/login.component.ts
+++ admin/src/app/containers/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/core/state/app.service';
-import { User } from 'src/core/state/app.model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,15 +11,12 @@ export class LoginComponent implements OnInit {
   form: FormGroup = new FormGroup({});
   uname = '';
   passwd = '';
-  return: string = '';
   constructor(
     private service: AppService,
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => console.log(params));
     this.initForm();
   }
 
@@ -33,9 +28,8 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    console.log(this.return);
     if (this.form.valid) {
-      // console.log(this.form.value);
+      console.log(this.form.value);
       this.service.updateUser(this.form.value);
       this.service.login(this.form.value);
     }
