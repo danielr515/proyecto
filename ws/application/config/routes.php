c734ecb6ff91f092b13607523a2c35dfbe1c225d
Daniel Rodriguez <daniel.rodriguez.iglesias9@gmail.com>
2020-05-13 18:23:15 +0200

    Subir php

--- ws/application/config/routes.php
+++ ws/application/config/routes.php
@@ -54,5 +54,6 @@ $route['loginAdmin'] = 'ws/WS_LoginController/loginAdmin';
 $route['logoutAdmin'] = 'ws/WS_LoginController/logoutAdmin';
 $route['registerAdmin'] = 'ws/WS_LoginController/registerAdmin';
 $route['types'] = 'ws/WS_TypesController/types';
+$route['typesWithWeakness'] = 'ws/WS_TypesController/typesWithWeakness';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
