eab2093dc959b0a22426ad7b827fc4b6807a39fc
Daniel Rodriguez <daniel.rodriguez.iglesias9@gmail.com>
2020-05-18 19:36:20 +0200

    Subir php

---
+++ ws/application/controllers/ws/WS_RoomsController.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once( APPPATH . 'controllers/ws/WS_MainController.php' );
+
+class WS_RoomsController extends WS_MainController {
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->model( 'room' );
+
+        $this->load->model( 'room' );
+
+    }
+
+    protected function createRoom_options() {
+        parent::setOptions();
+    }
+    protected function createRoom_post() {
+        $room = $this->post( 'room' );
+        $player = $this->post( 'player' );
+
+        $authorization = $this->input->get_request_header( 'Authorization' );
+        $token = explode( ' ', $authorization );
+        if ( count( $token ) > 1 ) {
+            $token = $token[1];
+        }
+        $retmsg = '';
+        $code = '';
+        if ( $room['name'] == '' || $room['type'] == '' || ( $room['type'] == 'PRIVATE' && $room['passwd'] == '' ) ) {
+            $retmsg = 'Faltan datos obligatorios';
+            $code = parent::HTTP_BAD_REQUEST;
+        } else {
+            if ( !isset( $player ) || !isset( $token ) ) {
+                $retmsg = 'Datos erróneos';
+                $code = parent::HTTP_UNAUTHORIZED;
+            } else {
+                if ( $this->player->userAndTokenValid( $player, $token ) ) {
+                    if ( !$this->room->playedAlreadyInRoom( $player ) ) {
+                        $return = $this->type->createRoom( $room, $player );
+                        if ( $return ) {
+                            $retmsg = 'Adición correcta';
+                            $code = parent::HTTP_OK;
+                        } else {
+                            $retmsg = 'Error al insertar';
+                            $code = parent::HTTP_INTERNAL_ERROR;
+                        }
+                    } else {
+                        $retmsg = 'Ya existe el tipo que estás intentando insertar';
+                        $code = parent::HTTP_BAD_REQUEST;
+                    }
+                } else {
+                    $retmsg = 'Datos erróneos';
+                    $code = parent::HTTP_UNAUTHORIZED;
+                }
+            }
+        }
+
+        $this->response( $retmsg, $code );
+    }
+
+}
