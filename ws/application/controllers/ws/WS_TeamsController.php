0892ed4a2d1e3efba765e8ea8605e2d44f58b875
Daniel Rodriguez <daniel.rodriguez.iglesias9@gmail.com>
2020-05-19 20:35:05 +0200

    Subir php

--- ws/application/controllers/ws/WS_TeamsController.php
+++ ws/application/controllers/ws/WS_TeamsController.php
@@ -7,6 +7,7 @@ class WS_TeamsController extends WS_MainController {
     public function __construct() {
         parent::__construct();
 
+        $this->load->model( 'player' );
         $this->load->model( 'team' );
     }
 
